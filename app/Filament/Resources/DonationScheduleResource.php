fc7cd803aa3c8f1399f183e68b3d876650419d28
amir-repository <achmadnassardhi@gmail.com>
2023-11-29 01:52:55 +0700

    fix wrong where query on donation schedule

--- app/Filament/Resources/DonationScheduleResource.php
+++ app/Filament/Resources/DonationScheduleResource.php
@@ -52,10 +52,10 @@ class DonationScheduleResource extends Resource
                 Filter::make('Today Donation')->query(fn (Builder $query): Builder => $query->whereDate('donation_date', Carbon::today()))->toggle(),
                 Filter::make('Tomorrow Donation')->query(fn (Builder $query): Builder => $query->whereDate('donation_date', Carbon::today()->addDays(1)))->toggle(),
                 Filter::make('Next-7 days Rescue')
-                    ->query(fn (Builder $query): Builder => $query->whereBetween('created_at', [Carbon::now(), Carbon::now()->addDays(7)]))
+                    ->query(fn (Builder $query): Builder => $query->whereBetween('donation_date', [Carbon::now(), Carbon::now()->addDays(7)]))
                     ->toggle(),
                 Filter::make('Next-30 days Rescue')
-                    ->query(fn (Builder $query): Builder => $query->whereBetween('created_at', [Carbon::now(), Carbon::now()->addDays(30)]))
+                    ->query(fn (Builder $query): Builder => $query->whereBetween('donation_date', [Carbon::now(), Carbon::now()->addDays(30)]))
                     ->toggle(),
                 SelectFilter::make('Volunteer')->relationship('user', 'name')->searchable()->preload(),
                 Filter::make('created_at')
